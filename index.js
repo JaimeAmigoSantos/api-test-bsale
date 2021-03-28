eed5da4117b688d55fb984e299dbc6d6de916a50
JaimeAmigoSantos <jaime.amigo@gmail.com>
2021-03-27 22:14:49 -0300

    Add cool face API

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // define a root route
 app.get('/', (req, res) => {
-  res.send("Hello Worlddddddddddddddddddddddddddddddddddddddddddddddddddddddddd");
+  res.send("Hello World");
 });
 
 app.use(cors());
